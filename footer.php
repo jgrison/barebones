2c83adead934eaf605c01a29122c12a24374cb29
Jeff <jgrison@vicimus.com>
2014-08-29 09:14:10 -0400

    Adding footer file

---
+++ footer.php
@@ -0,0 +1,5 @@
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="<?php echo get_stylesheet_uri(); ?>/js/main.js"></script>
+	</body>
+</html>
