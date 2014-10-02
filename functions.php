bd4281186d6d43e6b9ea6690aeada0a8ff56e219
Jeff Grison <jgrison@vicimus.com>
2014-10-02 16:59:46 -0400

    main-menu slug name

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_theme_support( 'menus' );
 if ( function_exists( 'register_nav_menus' ) ) {
   	register_nav_menus(
   		array(
-  		  'menu_slug' => 'Menu Name',
+  		  'main-menu' => 'Main Menu',
   		)
   	);
 }
