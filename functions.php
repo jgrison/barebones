e0b729a5bb575f2bd6840b1926e1745b8a636a1f
Jeff Grison <jgrison@vicimus.com>
2014-10-02 16:55:48 -0400

    adding menus back to the wp dashboard

--- functions.php
+++ functions.php
@@ -1,1 +1,11 @@
 <?php
+
+/* Menu option in Dashboard */
+add_theme_support( 'menus' );
+if ( function_exists( 'register_nav_menus' ) ) {
+  	register_nav_menus(
+  		array(
+  		  'menu_slug' => 'Menu Name',
+  		)
+  	);
+}
