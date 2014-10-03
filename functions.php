bc276d3d5a9dfa3e7eb9f92d680b9cbc1070801e
Jeff Grison <jgrison@vicimus.com>
2014-10-02 20:54:49 -0400

    Adding site title

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
 
+/* Add title to title tag */
+
+add_filter( 'wp_title', 'barebones_filter_wp_title' );
+function barebones_filter_wp_title( $title )
+{
+	return $title . esc_attr( get_bloginfo( 'name' ) );
+}
+
 /* Menu option in Dashboard */
+
 add_theme_support( 'menus' );
 if ( function_exists( 'register_nav_menus' ) ) {
   	register_nav_menus(
